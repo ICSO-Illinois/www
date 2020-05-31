5414724bd1f017127f7f211c90273b781bdd99ec
danielz02 <31590926+danielz02@users.noreply.github.com>
2020-05-31 03:58:38 -0500

    Remove the triangle in the header and change color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import Img from 'gatsby-image';
 import PropTypes from 'prop-types';
 
 const Wrapper = styled.header`
-  -webkit-clip-path: polygon(100% 0, 0 0, 0 70%, 50% 100%, 100% 70%);
-  clip-path: polygon(100% 0, 0 0, 0 70%, 50% 100%, 100% 70%);
+  -webkit-clip-path: polygon(0 0, 100% 0%, 100% 70%, 0% 70%);
+  clip-path: polygon(0 0, 100% 0%, 100% 70%, 0% 70%);
   @media (max-width: ${props => props.theme.breakpoints.s}) {
     -webkit-clip-path: polygon(100% 0, 0 0, 0 90%, 50% 100%, 100% 90%);
     clip-path: polygon(100% 0, 0 0, 0 90%, 50% 100%, 100% 90%);
