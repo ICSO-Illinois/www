5609504408b98c385bc207caedd2a2dbc7cfa8a1
danielz02 <31590926+danielz02@users.noreply.github.com>
2020-06-01 02:03:37 -0500

    Add campus map and merge incoming changes from master

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -36,7 +36,7 @@ const NavBar = () => (
       <Link to="/">主页</Link>
       <Link to="/blog">内容</Link>
       <Link to="/about">关于</Link>
-      <a href={"https://maps.danielz.ch/"}>地图</a>
+      <Link to={"/map"}>地图</Link>
     </Nav>
   </Headroom>
 );
---
+++ src/pages/map.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+import PropTypes from 'prop-types';
+import { Header, CampusMap } from '../components/';
+import { Layout } from '../layouts/';
+import styled from '@emotion/styled';
+
+const MapWrapper = styled.div`
+  body {
+    padding: 10%;
+    margin: 10%;
+  }
+  html, body, #app {
+    height: 100%;
+    width: 100vw;
+  }
+  h1,
+  h2,
+  p {
+    font-family: sans-serif;
+    text-align: center;
+  }
+  .leaflet-container {
+    height: 100vh;
+    width: 50vw;
+    margin: 0 auto;
+  }
+`
+
+const Map = () => (
+  <Layout>
+    <Helmet title={'ICSO 校园地图'} />
+    <Header title={'校园地图'} />
+    <MapWrapper>
+      <CampusMap lat={40.1091771} lng={-88.2270936}  zoom={50} />
+    </MapWrapper>
+  </Layout>
+)
+
+export default Map
+
+Map.prototype = {
+  center: PropTypes.object
+}
