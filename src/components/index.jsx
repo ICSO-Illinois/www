ab727d2c8170de5cb83c4f9f2432ddaab1c06418
danielz02 <31590926+danielz02@users.noreply.github.com>
2020-06-03 03:57:24 -0500

    Rename files to conform with conventions

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,6 +3,6 @@ import Header from './Header';
 import PostList from './PostList';
 import SEO from './SEO';
 import TagsBlock from './TagsBlock';
-import CampusMap from './map';
+import CampusMap from './Map';
 
 export { BlogList, Header, PostList, SEO, TagsBlock, CampusMap };
